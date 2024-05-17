f582c8f9b06e126e908bc10099f6c1f3d5b2376e
Mathias Gmeiner <mathias@weitweit.com>
2024-05-17 07:30:42 +0200

    Add sitemap integration and CustomPicture component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,23 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
   integrations: [
     tailwind({
       nesting: true,
     }),
+    sitemap(),
   ],
+  output: "hybrid",
   adapter: vercel({
     imageService: true,
   }),
   image: {
     domains: ["cms.klebermetzler.com", "cms.klebermetzler.test"],
   },
+  site: "https://beta.klebermetzler.com",
 });

0ea4118f0d4ca99addf7bb425de2af613f13d70e
Mathias Gmeiner <mathias@weitweit.com>
2024-05-23 14:50:56 +0200

    Refactor unnecessary console.log and update link rendering

--- src/middleware.js
+++ src/middleware.js
@@ -3,8 +3,6 @@ import { defineMiddleware } from "astro/middleware";
 export const onRequest = defineMiddleware((context, next) => {
   const hostname = context.url.hostname;
 
-  console.log(context);
-
   if (hostname === "klebermetzler.com") {
     return new Response(null, {
       status: 301,
